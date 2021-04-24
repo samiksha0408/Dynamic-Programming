3ecf7d89d7a3af429ad2c0bfbc7acd3d37358a71
samiksha0408 <50922918+samiksha0408@users.noreply.github.com>
2021-04-25 02:25:54 +0530

    Length of Shortest Common supersequence

---
+++ len_SCS.cpp
@@ -0,0 +1,45 @@
+#include "stdc++.h"
+
+using namespace std;
+
+int dp[1000][1000];
+
+
+
+int len_scs(string x, string y, int n, int m)
+{
+    
+    for(int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= m; j++)
+        {
+            if (i == 0 or j == 0) dp[i][j] = 0;
+
+            if (i > 0 and j > 0)
+            {
+                if(x[i - 1] == y[j - 1]) 
+                {
+                     dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                
+                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+               
+            }
+        }
+    }
+
+    int an = m+n-dp[n][m];
+    return an;
+}
+
+int main()
+{
+   // memset(dp, -1, sizeof(dp));
+    string x, y;
+    printf("Enter the two strings\n");
+    cin >> x >> y;
+
+    int ans = len_scs(x, y, x.length(), y.length());
+
+    cout << "Length of shortest common supersequence : " << ans << endl;
+}
