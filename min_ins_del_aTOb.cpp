6a63e43c611cd3e91e1bdbd2c01b9719e98f8741
samiksha0408 <50922918+samiksha0408@users.noreply.github.com>
2021-04-25 02:33:58 +0530

    Min dels and ins's to convert string x to y

---
+++ min_ins_del_aTOb.cpp
@@ -0,0 +1,39 @@
+#include "stdc++.h"
+using namespace std;
+int dp[1000][1000];
+int min_ins_del(string x, string y, int n, int m)
+{
+    for(int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= m; j++)
+        {
+            if (i == 0 or j == 0) dp[i][j] = 0;
+
+            if (i > 0 and j > 0)
+            {
+                if(x[i - 1] == y[j - 1]) 
+                {
+                     dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                
+                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+               
+            }
+        }
+    }
+
+    int an = m+n-2*dp[n][m];
+    return an;
+}
+
+int main()
+{
+   // memset(dp, -1, sizeof(dp));
+    string x, y;
+    printf("Enter the two strings\n");
+    cin >> x >> y;
+
+    int ans = min_ins_del(x, y, x.length(), y.length());
+
+    cout << "Minimum number of deletions and insertions to convert string a to b are : " << ans << endl;
+}
