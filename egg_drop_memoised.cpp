b51dd5b77cc8775c7655a5f703c193c47621f903
samiksha0408 <50922918+samiksha0408@users.noreply.github.com>
2021-05-07 01:02:52 +0530

    Egg Drop memoised

---
+++ egg_drop_memoised.cpp
@@ -0,0 +1,27 @@
+#include "stdc++.h"
+using namespace std;
+int dp[1000][1000];
+
+int min_at_in_worst_case(int e, int f)
+{
+    if(f == 0 or f == 1 or e == 1) return f; // minimum attempts in the worst case 
+    if(dp[e][f] != -1) return dp[e][f];
+
+    int ans = INT_MAX;
+    for(int k = 1; k <= f; k++)
+    {
+        
+        int tempans = 1 + max(min_at_in_worst_case(e-1,k-1), min_at_in_worst_case(e,f-k)); // max --> because worst case 
+        ans = min(tempans,ans);
+    }
+    return dp[e][f] = ans; 
+}
+int main()
+{
+    memset(dp,-1,sizeof(dp));
+    int e,f;
+    cout << "Enter the number of eggs and floors" << endl;
+    cin >> e >> f;
+    int ans = min_at_in_worst_case(e,f);
+    cout << "Minimum attempts in worst case to find the threshold floor are :" << ans << endl;
+}
